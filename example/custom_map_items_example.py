7c0eee84d8b63b56beb79e459d6a5d227edad1a0
cms26 <TMS8503@gmail.com>
2023-05-14 10:11:56 -0400

    Updated example to filter out custom items

--- example/custom_map_items_example.py
+++ example/custom_map_items_example.py
@@ -2,6 +2,14 @@ import requests
 
 url ='http://localhost/map-items/'
 
+def get_custom_items():
+    jsonItems = requests.get(url).json()
+    mapItems = []
+    for item in jsonItems:
+        if 'Custom-' in item['id']:
+            mapItems.append(item)
+    return mapItems
+    
 def put_data(update_data):
     print('Sending', update_data)
     requests.put(url, json=update_data)
@@ -9,15 +17,17 @@ def put_data(update_data):
 # add initial  set of map data
 json_data = [{"color": "#ff0000","location_history": [{"latitude_deg": 39.4227,"longitude_deg": -77.4187,"altitude_m": 534}, {"latitude_deg": 39.5227,"longitude_deg": -77.3187,"altitude_m": 734}]}, {"color": "#499287","location_history": [{"latitude_deg": 39.2227,"longitude_deg": -77.3337,"altitude_m": 534}, {"latitude_deg": 39.6627,"longitude_deg": -77.7787,"altitude_m": 734}]}]
 put_data(json_data)
-mapItems = requests.get(url).json()
 
+mapItems = get_custom_items()
+
+print('mapItems', mapItems)
 # change the color of the first item
 update_data = [{
     'id': mapItems[0]['id'],
     'color': '#34ebbd'
 }]
 put_data(update_data)
-mapItems = requests.get(url).json()
+mapItems = get_custom_items()
 
 # update the description of the first item
 update_data = [{
